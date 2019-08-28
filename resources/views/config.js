a550b66fc4783d42b24e3fafbd79ab4bbb71e710
Eric Reforma <ericreforma@gmail.com>
2019-08-28 15:57:43 +0800

    Previous working

--- resources/views/config.js
+++ resources/views/config.js
@@ -1,19 +1,36 @@
-const homepage = '';
+const homepage = '/TapAdsServer/public';
 
 export default {
-  api: {
-      campaignlist: `${homepage}/api/client/campaigns`,
-      createCampaign: `${homepage}/api/client/campaign/create`,
-      createGeoLocation: `${homepage}/api/client/campaign/new/geolocation`,
-      campaignDashboard: `${homepage}/api/client/campaign/dashboard/`,
-      getGeoLocation: `${homepage}/api/client/campaign/geolocation`,
-      submitRateUser: `${homepage}/api/client/user/rating`,
-      userProfileView: `${homepage}/api/client/user/:id/profile`,
-      login: `${homepage}/api/client/login`,
-      getLiveLocation: `${homepage}/api/client/campaign/getLiveLocation`,
-  },
-  apiKey: {
-      googleApiKey: 'AIzaSyBwXTceRsEryqeySF0HtM66cVwwWa9rW0w'
-  },
-  homepage
+    api: {
+        createCampaign: `${homepage}/api/client/campaign/create`,
+        createGeoLocation: `${homepage}/api/client/campaign/new/geolocation`,
+        campaignDashboard: (id) => {
+            return `${homepage}/api/client/campaign/dashboard/${id}`
+        },
+        getGeoLocation: `${homepage}/api/client/campaign/geolocation`,
+        getCampaignData: `${homepage}/api/client/campaign/location/data`,
+
+        submitRateUser: `${homepage}/api/client/user/rating`,
+        userProfileView: (id) => {
+            return `${homepage}/api/client/user/${id}/profile`
+        },
+        getChat: `${homepage}/api/client/user/chats`,
+        getConvo: `${homepage}/api/client/user/convo/`,
+        getTrips: (uid, cid) => {
+            return `${homepage}/api/client/user/trip/${cid}/${uid}`;
+        },
+        submitRateUser: `${homepage}/api/client/user/rating`,
+
+        updateNotif:`${homepage}/api/client/chat/notif/update`,
+
+        campaignList:  `${homepage}/api/client/campaigns`,
+        userRequests: `${homepage}/api/client/campaigns/requests`,
+        login: `${homepage}/api/client/login`,
+        clientNotification: `${homepage}/api/client/notifications`,
+
+        getListOfUsers: `${homepage}/api/client/chat/usersList`,
+    },
+    apiKey: {
+        googleApiKey: `AIzaSyBwXTceRsEryqeySF0HtM66cVwwWa9rW0w`
+    }
 };
